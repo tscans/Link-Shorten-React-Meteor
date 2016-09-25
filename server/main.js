a706436d83714e8a4205221877cc15aa9541bc3f
Tom Scanlan <tscanlan@mchs.org>
2016-09-25 15:15:10 -0500

    first

---
+++ server/main.js
@@ -0,0 +1,30 @@
+import { Meteor } from 'meteor/meteor';
+import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+  Meteor.publish('links', function(){
+  	return Links.find({});
+  });
+});
+
+function onRoute(req, res, next) {
+	const link = Links.findOne({ token: req.params.token });
+
+	if(link){
+		Links.update(link, { $inc: { clicks: 1 }});
+		res.writeHead(307, { 'location': link.url });
+		res.end();
+	}
+	else{
+		next();
+	}
+}
+
+const middleware = ConnectRoute(function(router){
+	router.get('/:token', onRoute);
+})
+
+WebApp.connectHandlers.use(middleware);
